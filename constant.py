7afc8947854d6769df28c69f041613a2cb006474
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2023-11-08 10:14:04 +0700

    Update Code

---
+++ constant.py
@@ -0,0 +1,26 @@
+class PygameConfig:
+    WIDTH = 640
+    HEIGHT = 480
+    TITLE = 'Pygame Example'
+    FPS = 60
+
+
+class Color:
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+
+
+class DinoSprite:
+    DINO_WALK_1 = './assets/dinosaur/Walk (1).png'
+    DINO_WALK_2 = './assets/dinosaur/Walk (2).png'
+    DINO_WALK_3 = './assets/dinosaur/Walk (3).png'
+    DINO_WALK_4 = './assets/dinosaur/Walk (4).png'
+    DINO_WALK_5 = './assets/dinosaur/Walk (5).png'
+    DINO_WALK_6 = './assets/dinosaur/Walk (6).png'
+    DINO_WALK_7 = './assets/dinosaur/Walk (7).png'
+    DINO_WALK_8 = './assets/dinosaur/Walk (8).png'
+    DINO_WALK_9 = './assets/dinosaur/Walk (9).png'
+    DINO_WALK_10 = './assets/dinosaur/Walk (10).png'
--- main.py
+++ main.py
@@ -3,33 +3,105 @@ import sys
 import pygame
 from pygame.locals import *
 import asyncio
+from constant import PygameConfig, Color, DinoSprite
 
 # Init constant
 pygame.init()
 
-fps = 60
+fps = PygameConfig.FPS
 fpsClock = pygame.time.Clock()
 
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption(PygameConfig.TITLE)
+screen = pygame.display.set_mode((PygameConfig.WIDTH, PygameConfig.HEIGHT))
+
+
+class Player:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.dinoSprite = [DinoSprite.DINO_WALK_1, DinoSprite.DINO_WALK_2, DinoSprite.DINO_WALK_3, DinoSprite.DINO_WALK_4, DinoSprite.DINO_WALK_5,
+                           DinoSprite.DINO_WALK_6, DinoSprite.DINO_WALK_7, DinoSprite.DINO_WALK_8, DinoSprite.DINO_WALK_9, DinoSprite.DINO_WALK_10]
+        self.delay = 0
+        self.image = None
+        self.spriteIndex = 0
+
+    def moveLeft(self):
+        self.updateSpriteIndex()
+        self.x -= 10
+        self.x = (self.x + PygameConfig.WIDTH) % PygameConfig.WIDTH
+
+    def moveRight(self):
+        self.updateSpriteIndex()
+        self.x += 10
+        self.x = (self.x + PygameConfig.WIDTH) % PygameConfig.WIDTH
+
+    def moveUp(self):
+        self.updateSpriteIndex()
+        self.y -= 10
+        self.y = (self.y + PygameConfig.HEIGHT) % PygameConfig.HEIGHT
+
+    def moveDown(self):
+        self.updateSpriteIndex()
+        self.y += 10
+        self.y = (self.y + PygameConfig.HEIGHT) % PygameConfig.HEIGHT
+
+    def drawRect(self):
+        pygame.draw.rect(screen, Color.BLUE,
+                         (self.x, self.y, self.width, self.height))
+
+    def drawImage(self, screen):
+        self.image = pygame.image.load(self.dinoSprite[self.spriteIndex])
+        self.image = pygame.transform.scale(self.image, (640, 480))
+        screen.blit(self.image, (self.x, self.y))
+        pass
+
+    def update(self):
+        # self.moveRight()
+        pass
+
+    def updateSpriteIndex(self):
+        self.spriteIndex += 1
+        if self.spriteIndex >= len(self.dinoSprite):
+            self.spriteIndex = 0
 
 
 async def main():
+    # Create player
+    player = Player(100, 0, 50, 50)
+
     running = True
 
     # Game loop.
     while running:
-        screen.fill((0, 0, 0))
+        screen.fill(Color.WHITE)
         # Events
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                elif event.key == pygame.K_LEFT:
+                    player.moveLeft()
+                elif event.key == pygame.K_RIGHT:
+                    player.moveRight()
+                elif event.key == pygame.K_UP:
+                    player.moveUp()
+                elif event.key == pygame.K_DOWN:
+                    player.moveDown()
 
         if running == False:
             break
-        # Update Logic
 
+        # Logic
+        player.update()
         # Draw
+        player.drawRect()
+        player.drawImage(screen)
 
         pygame.display.flip()
         fpsClock.tick(fps)
